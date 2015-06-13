caabbf11cc05fd154bbedd354f6c8fd23066f65f
CzajaM <marcinc7654321@gmail.com>
2015-06-13 16:54:58 +0200

    Test class Priority.java #2

--- ModularOrganizer/src/test/testPriority.java
+++ ModularOrganizer/src/test/testPriority.java
@@ -16,43 +16,94 @@ public class testPriority {
 	Priority priority;
 
 	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	public static void setUpBeforeClass() throws Exception
+	{
 		System.out.println("Test class: Priority.java");
 	}
 
 	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
+	public static void tearDownAfterClass() throws Exception
+	{
 		System.out.println("End of Test: Priority.java");
 	}
 
 	@Before
-	public void setUp() throws Exception {
-		priority = new Priority(2, "Test");
+	public void setUp() throws Exception
+	{
+		priority = new Priority(2, "Martin");
 	}
-
+		
 	@After
-	public void tearDown() throws Exception {
+	public void tearDown() throws Exception
+	{
 		priority = null;
 	}
 
 	@Test
 	public void testPriorityFirst() {
+			
+		assertEquals("Fail: variable level", 2, priority.getLevel());
+		assertEquals("Fail: variable name", "Martin", priority.getName());
+	}
 		
-		assertEquals("Fail: variable level", 2, priority.level);
-		assertEquals("Fail: variable name", "Test", priority.name);
-	}
-	
 	@Test
-	public void testPrioritySecond() {
-	    Priority priority2 = new Priority(2, "Test");
+	public void testPrioritySecond() 
+	{
+	    Priority priority2 = new Priority(2, "Martin");
 	    
-	    assertEquals("Fail: variable level", priority2.level, priority.level);
-		assertEquals("Fail: variable name", priority2.name, priority.name);
+	    assertEquals("Fail: variable level", priority2.getLevel(), priority.getLevel());
+		assertEquals("Fail: variable name", priority2.getName(), priority.getName());
+	}
+		
+	@Test
+	public void testPriorityThird()
+	{
+		assertEquals("Fail: variable level", priority.getLevel(), priority.getLevel());
+		assertEquals("Fail: variable name", priority.getName(), priority.getName());
+	}
+
+
+	@Test
+	public void testGetLevel()
+	{
+		assertEquals("Fail: variable level", priority.getLevel(), 2);
+	}
+
+	@Test
+	public void testSetLevelFirst()
+	{
+		assertEquals("Fail: variable level", priority.getLevel(), 2);
+		priority.setLevel(3);
+		assertEquals("Fail: variable level", priority.getLevel(), 3);
 	}
 	
 	@Test
-	public void testPriorityThird() {
-		assertEquals("Fail: variable level", priority.level, priority.level);
-		assertEquals("Fail: variable name", priority.name, priority.name);
+	public void testSetLevelSecond()
+	{
+		int level=3;
+		priority.setLevel(3);
+		assertEquals("Fail: variable level", priority.getLevel(), level);
+	}
+
+	@Test
+	public void testGetName()
+	{
+		assertEquals("Fail: variable name", priority.getName(), "Martin");
+	}
+
+	@Test
+	public void testSetNamFirst()
+	{
+		assertEquals("Fail: variable name", priority.getName(), "Martin");
+		priority.setName("Jon");
+		assertEquals("Fail: variable name", priority.getName(), "Jon");
+	}
+
+	@Test
+	public void testSetNamSecond()
+	{
+		String name="Jon";
+		priority.setName("Jon");
+		assertEquals("Fail: variable name", priority.getName(), name);
 	}
 }
