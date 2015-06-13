7d9135580962f6e2f2f2915c774ad80777b2b71d
Damian Piela <damianp.zg@gmail.com>
2015-06-13 16:18:40 +0200

    seems final

--- ModularOrganizer/src/model/PrioritiesData.java
+++ ModularOrganizer/src/model/PrioritiesData.java
@@ -6,10 +6,19 @@ import java.util.ArrayList;
 
 public class PrioritiesData implements Serializable {
 	
-	public ArrayList<Priority> data;
+	private ArrayList<Priority> data;
 	
 	public PrioritiesData(){
 		this.data = new ArrayList<Priority>();
+	}//end of constructor
+	
+	//getters and setters
+	public ArrayList<Priority> getData() {
+		return data;
+	}
+
+	public void setData(ArrayList<Priority> data) {
+		this.data = data;
 	}
 	
 }//end of class
