d6e9892c43eca1af6b15a0969bd357c1c921e362
Damian Piela <damianp.zg@gmail.com>
2015-06-18 19:08:39 +0200

    Dodano opisy do dokumentacji

--- ModularOrganizer/src/model/PrioritiesData.java
+++ ModularOrganizer/src/model/PrioritiesData.java
@@ -4,10 +4,19 @@ package model;
 import java.io.Serializable;
 import java.util.ArrayList;
 
+/**
+ * A serializable library for storing Priority-type objects. 
+ * 
+ * @author Damian Piela
+ *
+ */
 public class PrioritiesData implements Serializable {
 	
 	private ArrayList<Priority> data;
 	
+	/**
+	 * Constructor for the PrioritiesData class
+	 */
 	public PrioritiesData(){
 		this.data = new ArrayList<Priority>();
 	}//end of constructor
