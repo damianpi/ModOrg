4cefb94bd7ebb241526ce8951f57ce4002365a3f
Damian Piela <damianp.zg@gmail.com>
2015-06-18 19:07:35 +0200

    Dodano opisy do dokumentacji

--- ModularOrganizer/src/model/ContactsData.java
+++ ModularOrganizer/src/model/ContactsData.java
@@ -5,11 +5,20 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import javax.swing.DefaultListModel;
 
+/**
+ * Creates a serializable library consisting of Contact-type objects. Includes an array list of Contact objects  
+ *  as well as list model of Strings to use with the view's main pane.
+ * @author Damian Piela
+ *
+ */
 public class ContactsData implements Serializable{
 	
 	private ArrayList<Contact> data;
 	private DefaultListModel listModel;
 	
+	/**
+	 * Constructor for the ContactsData class.
+	 */
 	public ContactsData(){
 		this.data = new ArrayList<Contact>();
 		this.listModel = new DefaultListModel();	
